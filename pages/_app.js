8104179b906be64e0efee7f682771abeae6e2045
Tae Lee <taekyu.lee3@gmail.com>
2022-11-15 15:22:20 +1100

    portfolio FAVICON

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,17 @@ import 'styles/projects.css'
 import 'styles/navbar.css'
 import 'styles/global.css'
 import 'styles/aboutme.css'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/images/favicon.ico" />
+
+      </Head>
+      <Component {...pageProps} />
+
+    </>
+  )
 }
