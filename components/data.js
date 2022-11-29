86aea420223a28975b010d8189d2d422300718a9
Tae Lee <taekyu.lee3@gmail.com>
2022-11-30 01:57:58 +1100

    fixed card sizing

--- components/data.js
+++ components/data.js
@@ -6,7 +6,7 @@ export default [
     url: "https://puri.vercel.app/",
     path: "azuki-mint.jpg",
     description: "Responsive website (Web3 / NFT mint site)",
-    skills: "React.js (Next.js), HTML, Tailwind CSS, Sanity, Third-web and Typescript"
+    skills: "Next.js, HTML, Tailwind CSS, Sanity and Typescript"
   },
   {
     id: 2,
@@ -14,7 +14,7 @@ export default [
     url: "https://ftxclassic.io/",
     path: "FTTC.jpg",
     description: "Responsive website (Web3 / ERC20 token)",
-    skills: "React.js (Next.js), HTML, CSS and Javascript"
+    skills: "Next.js, HTML, CSS and Javascript"
   },
   {
     id: 3,
@@ -30,7 +30,7 @@ export default [
     url: "https://www.music-mates.com/",
     path: "musicmates.png",
     description: "Mobile-only application (social platform)",
-    skills: "Ruby on Rails, HTML, PostgreSQL, Sass, Javascript and Heroku"
+    skills: "Ruby on Rails, HTML, PostgreSQL, Sass and Javascript"
   },
   {
     id: 5,
@@ -38,6 +38,6 @@ export default [
     url: "https://come-out-of-the-closet.herokuapp.com/",
     path: "comeoutofthecloset.png",
     description: "Desktop-only application (online marketplace)",
-    skills: "Ruby on Rails, HTML, PostgreSQL, Sass, Javascript and Heroku"
+    skills: "Ruby on Rails, HTML, PostgreSQL, Sass and Javascript"
   }
 ]
